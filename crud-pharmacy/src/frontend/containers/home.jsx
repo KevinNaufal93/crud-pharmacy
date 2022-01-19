4528f7bb32a6b9c3aa29d8420b34302ecb0f3860
MorningToast21 <Kevinnp28@gmail.com>
2022-01-19 12:31:33 +0700

    finished homepage/login layout and default / directory

---
+++ crud-pharmacy/src/frontend/containers/home.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Link } from "react-router-dom";
+import '../assets/styles/home.css';
+
+export class Home extends React.Component {
+
+    state = {
+        email: "",
+        password: ""
+    }
+
+    inputHandler = (event) => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({ [name]: value })
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="home-header">
+                    Welcome to Kevin's Apothecary Website
+                </div>
+                <Form className="home-login-form">
+                    <FormGroup>
+                        <Label for="userEmail">Email</Label>
+                        <Input type="email" name="email" placeholder="enter your email" onChange={this.inputHandler}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="userPassword">Password</Label>
+                        <Input type="password" name="password" placeholder="enter your password" onChange={this.inputHandler}/>
+                    </FormGroup>
+                    <Button>Login Now!</Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default Home
